d5314dcfcbef0108f4369e8d01fb0357b9294039
hunknownz <klercc37@gmail.com>
2020-10-21 08:01:02 +0800

    commit job_02 of week01
    Signed-off-by: hunknownz <klercc37@gmail.com>

---
+++ Week_01/jobs/job_01_01/hello/src/HelloByteCode.java
@@ -0,0 +1,8 @@
+public class HelloByteCode {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("a + b =" + sum);
+    }
+}
